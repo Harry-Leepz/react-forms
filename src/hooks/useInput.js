e1b5f99fe58d93ed89426f79481c416b6cc33f4d
Harry Dhillon <hdhillon478@gmail.com>
2022-10-15 19:10:47 +0100

    Add logic to useInput custom hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,8 +1,26 @@
 import { useState } from "react";
 
-const useInput = () => {
+const useInput = (validateValue) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangehandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const valueBlurhandler = (event) => {
+    setIsTouched(true);
+  };
+
+  return {
+    value: enteredValue,
+    hasError: hasError,
+    valueChangehandler: valueChangehandler,
+    valueBlurhandler: valueBlurhandler,
+  };
 };
 
 export default useInput;
