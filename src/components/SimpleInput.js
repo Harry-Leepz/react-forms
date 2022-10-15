74cf9e26d04bb2c69b715373899dfdb7ea5a64e9
Harry Dhillon <hdhillon478@gmail.com>
2022-10-15 09:06:55 +0100

    Add validation error message on empty form input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,23 +1,18 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 /*
-  A basic form component used as an example. 
-  It is used to show two method of capturing form data,
-  1. Using state
-  2. Use refs.
+  A form component with a single input, 
+  used to test input capturing and validation
 */
 
 const SimpleInput = (props) => {
-  // 1. Using state method
   const [enteredName, setEnteredName] = useState("");
+  const [inputIsValid, setinputIsValid] = useState(true);
 
   // set state to the target value bound to this function
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
-
-  // 2. Using refs
-  const nameInputRef = useRef();
 
   // handler for form submission
   const formSubmitHandler = (event) => {
@@ -25,30 +20,31 @@ const SimpleInput = (props) => {
 
     // form input validation
     if (enteredName.trim() === "") {
+      setinputIsValid(false);
       return;
     }
 
+    setinputIsValid(true);
     console.log("state value", enteredName);
-
-    // get current ref value
-    const nameInputRefValue = nameInputRef.current.value;
-    console.log("ref value", nameInputRefValue);
-
     // reset form input fields using two way data binding
     setEnteredName("");
   };
 
+  const nameInputCssClasses = inputIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputCssClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-          ref={nameInputRef}
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!inputIsValid && <p className='error-text'>Please input a name!</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
