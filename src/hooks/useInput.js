51c511f875457f10d2647f2b6acf66b410e22f05
Harry Dhillon <hdhillon478@gmail.com>
2022-10-15 19:15:05 +0100

    Add comments to useinput custom hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
 
+/*
+  useIput custom hook, 
+  takes one paramenter (function to validate user input)
+  returns object {
+  1. entered user input value,
+  2. does the input value have an error,
+  3. changeHandler function to update state with value,
+  4. blueHndler to check if input was touched
+  }
+*/
+
 const useInput = (validateValue) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isTouched, setIsTouched] = useState(false);
 
+  // input validation
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
 
+  // update state
   const valueChangehandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
+  // check if input is touched
   const valueBlurhandler = (event) => {
     setIsTouched(true);
   };
